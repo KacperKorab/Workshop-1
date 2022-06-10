1acca6d9c77a5fb8febf9c669961eb5e431a5b72
KacperKorab <kacperkorab8@gmail.com>
2022-06-10 10:09:56 +0200

    Test commit and push

--- Workshop-1/src/main/java/pl/coderslab/TaskManager.java
+++ Workshop-1/src/main/java/pl/coderslab/TaskManager.java
@@ -1,4 +1,18 @@
 package pl.coderslab;
 
+
+/*
+TODO wyświetlanie wszystkich dostępnych zadań,
+TODO wyjście z aplikacji,
+TODO dodanie zadania,
+TODO usuwanie zadania,
+TODO wczytywanie danych z pliku przy starcie aplikacji,
+TODO zapis danych do pliku,
+TODO sprawdzanie poprawność wartości liczbowej podczas usuwania.
+ */
+
 public class TaskManager {
+    public static void main(String[] args) {
+        System.out.println(ConsoleColors.RED + " red ");
+    }
 }
