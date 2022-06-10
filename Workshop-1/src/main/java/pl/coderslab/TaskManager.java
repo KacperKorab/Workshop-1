3b7566601b7747e4e3a65069f4b374a8e67a83e8
KacperKorab <kacperkorab8@gmail.com>
2022-06-10 11:38:13 +0200

    list

--- Workshop-1/src/main/java/pl/coderslab/TaskManager.java
+++ Workshop-1/src/main/java/pl/coderslab/TaskManager.java
@@ -11,8 +11,11 @@ TODO zapis danych do pliku,
 TODO sprawdzanie poprawność wartości liczbowej podczas usuwania.
  */
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
 import static java.lang.System.exit;
 
 public class TaskManager {
@@ -20,27 +23,28 @@ public class TaskManager {
         System.out.println(ConsoleColors.CYAN_BOLD + "WELCOME!");
         System.out.println(ConsoleColors.RESET);
 
-        // Menu Navigation
-        String[] options = {"1- Option 1", "2- Option 2", "3- Option 3", "4- Exit"};
+        List<List<String>> records = csvIntoList();// CSV Into ArrayList with print
+
+        // User Menu
+        String[] options = {"1- Add 1", "2- Remove 2", "3- List 3", "4- Exit"};
         Scanner scanner = new Scanner(System.in);
         int option = 0;
-
         while (option != 4){ // menu loop
             printMenu(options); //print menu method
                 try {
                 option = scanner.nextInt();
-                    switch (option) {
-                        case 1 -> option1();
-                        case 2 -> option2();
-                        case 3 -> option3();
+                    switch (option) {// Menu Options
+                        case 1 -> add();
+                        case 2 -> remove();
+                        case 3 -> list(records);
                         case 4 -> {System.out.println(ConsoleColors.WHITE_BOLD_BRIGHT+"Program terminated by user");exit(0);}
                     }
-                    if (option > options.length) {
+                    if (option > options.length) { // User selected option larger than 4
                         System.out.println(ConsoleColors.RED+"Incorrect input! Please enter an integer value between 1 and " + options.length);
                         System.out.print(ConsoleColors.RESET);
                     }
                 }
-            catch (InputMismatchException ex){
+            catch (InputMismatchException ex){ // User input other than int
                 System.out.println(ConsoleColors.RED+"Incorrect input! Please enter an integer value between 1 and " + options.length);
                 System.out.print(ConsoleColors.RESET);
                 scanner.next();
@@ -49,18 +53,22 @@ public class TaskManager {
                 System.out.println("An unexpected error happened. Please try again");
                 scanner.next();
             }
-        }
+        } // User Menu end
     }
 
-    // Menu Options
-    private static void option1() {
-        System.out.println("You chose option 1.");
-    }
-    private static void option2() {
-        System.out.println("You chose option 2.");
-    }
-    private static void option3() {
-        System.out.println("You chose option 3.");
+    private static List<List<String>> csvIntoList() { // CSV Into ArrayList with print
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("tasks.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                records.add(Arrays.asList(values));
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return records;
     }
 
     // User Menu
@@ -69,6 +77,20 @@ public class TaskManager {
             System.out.println(option);
         }
         System.out.print("Choose your option : ");
+    }
+
+    // Menu Options
+    private static void add() {
+        System.out.println("You chose option 1.");
+    }
+    private static void remove() {
+        System.out.println("You chose option 2.");
+    }
+    private static void list(List<List<String>> records) {
+        System.out.println("You chose option 3.");
+        for (List<String> printList : records) {
+            System.out.println(printList);
+        }
     }
 }
 
