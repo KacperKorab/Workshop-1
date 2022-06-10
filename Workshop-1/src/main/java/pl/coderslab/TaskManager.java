d7dbc17715c43c531b305169813a22129660d9ca
KacperKorab <kacperkorab8@gmail.com>
2022-06-10 14:13:23 +0200

    Added add and remove functionality, TODO date input checks

--- Workshop-1/src/main/java/pl/coderslab/TaskManager.java
+++ Workshop-1/src/main/java/pl/coderslab/TaskManager.java
@@ -4,11 +4,13 @@ package pl.coderslab;
 /*
 DONE wyświetlanie wszystkich dostępnych zadań,
 DONE wyjście z aplikacji,
-TODO dodanie zadania,
-TODO usuwanie zadania,
+DONE dodanie zadania, TODO check for valid date input
+Done usuwanie zadania,
 DONE wczytywanie danych z pliku przy starcie aplikacji,
 TODO zapis danych do pliku,
 TODO sprawdzanie poprawność wartości liczbowej podczas usuwania.
+
+TODO test incorrect input
  */
 
 import java.io.BufferedReader;
@@ -26,30 +28,33 @@ public class TaskManager {
         List<List<String>> records = csvIntoList();// CSV Into ArrayList with print
 
         // User Menu
-        String[] options = {"1- Add 1", "2- Remove 2", "3- List 3", "4- Exit"};
+        String[] options = {"1- Add", "2- Remove", "3- List", "4- Exit"};
         Scanner scanner = new Scanner(System.in);
         int option = 0;
-        while (option != 4){ // menu loop
+        while (option != 4) { // menu loop
             printMenu(options); //print menu method
-                try {
+            try {
                 option = scanner.nextInt();
-                    switch (option) {// Menu Options
-                        case 1 -> add();
-                        case 2 -> remove();
-                        case 3 -> list(records);
-                        case 4 -> {System.out.println(ConsoleColors.WHITE_BOLD_BRIGHT+"Program terminated by user");exit(0);}
-                    }
-                    if (option > options.length) { // User selected option larger than 4
-                        System.out.println(ConsoleColors.RED+"Incorrect input! Please enter an integer value between 1 and " + options.length);
-                        System.out.print(ConsoleColors.RESET);
+                switch (option) {// Menu Options
+                    case 1 -> add(records);
+                    case 2 -> remove(records);
+                    case 3 -> list(records);
+                    case 4 -> {
+                        System.out.println(ConsoleColors.WHITE_BOLD_BRIGHT + "Program terminated by user");
+                        exit(0);
                     }
                 }
-            catch (InputMismatchException ex){ // User input other than int
-                System.out.println(ConsoleColors.RED+"Incorrect input! Please enter an integer value between 1 and " + options.length);
+                if (option > options.length) { // User selected option larger than 4
+                    System.out.println(ConsoleColors.RED + "Incorrect input! Please enter an integer value between 1 and " + options.length);
+                    System.out.print(ConsoleColors.RESET);
+                }
+            }
+            catch (InputMismatchException ex) { // User input other than int
+                System.out.println(ConsoleColors.RED + "Incorrect input! Please enter an integer");
                 System.out.print(ConsoleColors.RESET);
                 scanner.next();
             }
-            catch (Exception ex){
+            catch (Exception ex) {
                 System.out.println("An unexpected error happened. Please try again");
                 scanner.next();
             }
@@ -72,35 +77,63 @@ public class TaskManager {
     }
 
     // User Menu
-    public static void printMenu(String[] options){
-        for (String option : options){
+    public static void printMenu(String[] options) {
+        for (String option : options) {
             System.out.println(option);
         }
         System.out.print("Choose your option : ");
     }
 
     // Menu Options
-    private static void add() {
-        System.out.println("You chose option 1. Press 1 to go back to menu.");
-        backToMenu();
+    private static void add(List<List<String>> records) {
+        System.out.println("Enter task name:");
+
+        Scanner addScanner = new Scanner(System.in);
+        List<String> taskList = new ArrayList<>();
+        String userInput = String.valueOf(addScanner.nextLine());
+        taskList.add(userInput);
+
+        System.out.println("Enter task deadline in yyyy-mm-dd format:"); //TODO check for proper input
+        String addDate = String.valueOf(addScanner.nextLine());
+        taskList.add(addDate);
+
+        System.out.println("Is this task important? Enter '1' for yes or '2' for no.");
+        int addTF = 0;
+        while (addTF != 1 && addTF != 2) {
+            addTF = addScanner.nextInt();
+            switch (addTF) {
+                case 1 -> taskList.add("true");
+                case 2 -> taskList.add("false");
+            }
+        }
+        records.add(taskList);
     }
-    private static void remove() {
-        System.out.println("You chose option 2. Press 1 to go back to menu.");
-        backToMenu();
+
+    private static void remove(List<List<String>> records) {
+        System.out.println("To remove a task enter it's number:");
+        Scanner removeScanner = new Scanner(System.in);
+        int userInput = (removeScanner.nextInt());
+        records.remove(userInput);
     }
+
     private static void list(List<List<String>> records) {
-        System.out.println("You chose option 3. Press 1 to go back to menu.");
-        for (List<String> printList : records) {
-            System.out.println(printList);
+        for (int i = 0; i < records.size(); i++) {
+            List<String> printList = records.get(i);
+            System.out.println(i + " : " + printList);
         }
         backToMenu();
     }
 
     private static void backToMenu() { //Waits for user input, then goes back to menu
+        System.out.println("Press 4 to go back to menu.");
         Scanner listScanner = new Scanner(System.in);
         int option = 0;
-        while (option != 1) {
+        while (option != 4) {
             option = listScanner.nextInt();
+            if (option != 4) { // User selected option other than 4
+                System.out.println(ConsoleColors.RED + "Incorrect input! Press 4 to go back to menu.");
+                System.out.print(ConsoleColors.RESET);
+            }
         }
     }
 }
